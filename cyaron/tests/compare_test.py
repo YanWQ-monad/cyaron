739976510d6a9a7623f473256a5d212129d1dedd
YanWQ-monad <YanWQmonad@gmail.com>
2019-09-02 21:33:56 +0800

    refactor compare_test with assertRaises

--- cyaron/tests/compare_test.py
+++ cyaron/tests/compare_test.py
@@ -47,17 +47,16 @@ class TestCompare(unittest.TestCase):
         with open("test_another_incorrect.out", "wb") as f:
             f.write(b"test123\r\ntest124 ")
 
-        try:
+        with self.assertRaises(CompareMismatch) as cm:
             with captured_output() as (out, err):
                 Compare.output("test_another_incorrect.out", std=io)
-        except CompareMismatch as e:
-            self.assertEqual(e.name, 'test_another_incorrect.out')
-            e = e.mismatch
-            self.assertEqual(e.content, 'test123\r\ntest124 ')
-            self.assertEqual(e.std, 'test123 \ntest123\n\n')
-            self.assertEqual(str(e), 'On line 2 column 7, read 4, expected 3.')
-        else:
-            self.assertTrue(False)
+
+        e = cm.exception
+        self.assertEqual(e.name, 'test_another_incorrect.out')
+        e = e.mismatch
+        self.assertEqual(e.content, 'test123\r\ntest124 ')
+        self.assertEqual(e.std, 'test123 \ntest123\n\n')
+        self.assertEqual(str(e), 'On line 2 column 7, read 4, expected 3.')
 
         result = out.getvalue().strip()
         self.assertEqual(result, "test_another_incorrect.out: !!!INCORRECT!!! On line 2 column 7, read 4, expected 3.")
@@ -75,18 +74,17 @@ class TestCompare(unittest.TestCase):
 
         io.output_writeln("1")
 
-        try:
+        with self.assertRaises(CompareMismatch) as cm:
             with captured_output() as (out, err):
                 Compare.program("python correct.py", "python incorrect.py", std=io, input=io, grader="FullText")
-        except CompareMismatch as e:
-            self.assertEqual(e.name, 'python incorrect.py')
-            e = e.mismatch
-            self.assertEqual(e.content, '2\n')
-            self.assertEqual(e.std, '1\n')
-            self.assertEqual(e.content_hash, '53c234e5e8472b6ac51c1ae1cab3fe06fad053beb8ebfd8977b010655bfdd3c3')
-            self.assertEqual(e.std_hash, '4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865')
-        else:
-            self.assertTrue(False)
+
+        e = cm.exception
+        self.assertEqual(e.name, 'python incorrect.py')
+        e = e.mismatch
+        self.assertEqual(e.content, '2\n')
+        self.assertEqual(e.std, '1\n')
+        self.assertEqual(e.content_hash, '53c234e5e8472b6ac51c1ae1cab3fe06fad053beb8ebfd8977b010655bfdd3c3')
+        self.assertEqual(e.std_hash, '4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865')
 
         result = out.getvalue().strip()
         correct_out = 'python correct.py: Correct \npython incorrect.py: !!!INCORRECT!!! Hash mismatch: read 53c234e5e8472b6ac51c1ae1cab3fe06fad053beb8ebfd8977b010655bfdd3c3, expected 4355a46b19d348dc2f57c046f8ef63d4538ebb936000f3c9ee954a27460dd865'
@@ -136,9 +134,5 @@ class TestCompare(unittest.TestCase):
     def test_timeout(self):
         if sys.version_info >= (3, 3):
             with IO() as test:
-                try:
+                with self.assertRaises(subprocess.TimeoutExpired):
                     Compare.program(((sys.executable, '-c', '__import__(\'time\').sleep(10)'), 1), std=test, input=test)
-                except subprocess.TimeoutExpired:
-                    pass
-                else:
-                    self.assertTrue(False)
